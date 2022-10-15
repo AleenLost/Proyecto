d0d0585efed6f9f7e54c2340a9ff6d6236467f6f
AleenLost <kyaraortiz57@gmail.com>
2022-10-14 23:41:05 -0600

    cambio5

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const config={
+const dotenv=require('dotenv').config();
+
+module.exports={
     NODE_ENV:process.env.NODE_ENV || 'development',
     PORT:process.env.PORT || 3000,
     DB_HOST:process.env.DB_HOST || 'localhost',
@@ -8,4 +10,3 @@ const config={
     DB_PORT: process.env.DB_PORT || 3306
 };
 
-module.exports=config;
