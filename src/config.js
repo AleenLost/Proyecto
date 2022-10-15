3991624c6275224ba5d71d59ae489243e0088ac0
AleenLost <kyaraortiz57@gmail.com>
2022-10-14 23:26:58 -0600

    cambio5

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config={
+    NODE_ENV:process.env.NODE_ENV || 'development',
     PORT:process.env.PORT || 3000,
     DB_HOST:process.env.DB_HOST || 'localhost',
     DB_USER:process.env.DB_USER || 'root',
